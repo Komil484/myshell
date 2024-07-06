106cee77c3f0135ce16b54cfd9054cfadc6b50ba
Komil Tokhirov <komil.tokhirov@gmail.com>
2024-07-06 14:40:28 +0500

    removed debug info in mysh executable

--- source/main.c
+++ source/main.c
@@ -7,32 +7,16 @@
 int main(void)
 {
     char *input = get_input();
-    unsigned long input_len = strlen(input);
-    char *input_copy = malloc(input_len + 1);
-    input_copy = strcpy(input_copy, input);
 
-    char **parsed = parse(input); // modifies input
+    char **parsed = parse(input);
     
-    
-    printf("Input: %s\n", input_copy);
+    free(input);
 
-    printf("Resulting: ");
     for (unsigned long i = 0; parsed[i] != NULL; ++i) { 
         printf("[%s] ", parsed[i]);
     }
     printf("\n");
     
-    
-    printf("Memory: ");
-    for (unsigned long i = 0; i < input_len + 1; ++i) {
-        char c = parsed[0][i];
-        if (c) {
-            printf("%c", c);
-        } else {
-            printf("0");
-        }
-    }
-    printf("\n");
 
     return 0;
 }   
